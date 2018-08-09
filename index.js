8ecafb61e53faa10ebafd5eaa175b707e3822e58
Mhaventhan Arun <MhaventhanArun@mhaventhans-mbp.home>
2018-08-09 21:34:48 +0100

    displya function added

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 var calNumb = document.getElementsByClassName('buttonNum');
 var disScreen = document.getElementById('screen');
 var opSelect = document.getElementsByClassName('operator');
+
+function display() {
+  disScreen.innerHTML = "";
+  disScreen.innerHTML += this.innerHTML;
+  }
