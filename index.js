fa0a410cddb880d4db0c22e01ae819903d4ec6ec
Mhaventhan Arun <MhaventhanArun@mhaventhans-mbp.home>
2018-08-09 22:50:12 +0100

    created global variable to assing values

--- index.js
+++ index.js
@@ -1,8 +1,35 @@
+var num1,num2,operator;
+
+// creating a variables to get element using DOM and store it under variable names
 var calNumb = document.getElementsByClassName('buttonNum');
 var disScreen = document.getElementById('screen');
 var opSelect = document.getElementsByClassName('operator');
 
-function display() {
-  disScreen.innerHTML = "";
-  disScreen.innerHTML += this.innerHTML;
+
+
+  for (var i = 0; i < calNumb.length; i++) {
+    calNumb[i].addEventListener('click',function(){
+      if (disScreen.innerHTML == "Sparta Calculator") {
+        disScreen.innerHTML = this.innerHTML;
+      }else{
+        disScreen.innerHTML += this.innerHTML;
+      }
+    });
+
   }
+
+// after pressing operator
+// save the value from the screen into num1
+var num1= disScreen.innerHTML;
+// disScreen.innerHTML= "";
+// save the operator into operator
+
+
+// if user presses equals
+
+// assigned num2 the discreen.innerHTML
+var num2= disScreen.innerHTML;
+// =
+  function addition(num1, operator, num2) {
+
+}
